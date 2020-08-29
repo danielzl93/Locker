5f31d96ed6ee7e5b460a798b2e8c8b073fc2b15f
danielzl93 <zhangliang1217@gmail.com>
2020-08-29 14:52:22 +0800

    [liangzhang] feat: should return no capacity when locker has no capacity

--- src/main/java/cn/xpbootcamp/locker/Locker.java
+++ src/main/java/cn/xpbootcamp/locker/Locker.java
@@ -1,13 +1,21 @@
 package cn.xpbootcamp.locker;
 
 public class Locker {
-    int capacity;
+    private int capacity;
 
     public Locker(int capacity) {
         this.capacity = capacity;
     }
 
     public String store() {
+        if (capacity < 1) {
+            return "no capacity";
+        }
+        capacity -= 1;
         return "barcode_01";
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 }
--- src/test/java/cn/xpbootcamp/locker/StoreTest.java
+++ src/test/java/cn/xpbootcamp/locker/StoreTest.java
@@ -5,11 +5,24 @@ import org.junit.Test;
 
 public class StoreTest {
     @Test
-    public void should_return_ticket_when_locker_has_capacity() {
+    public void should_return_ticket_and_capacity_decrease_1_when_locker_has_capacity() {
         String expectTicket = "barcode_01";
-        Locker locker = new Locker(10);
+        int capacity = 10;
+        Locker locker = new Locker(capacity);
 
         String ticket = locker.store();
         Assert.assertEquals(expectTicket, ticket);
+        Assert.assertEquals(capacity - 1, locker.getCapacity());
+    }
+
+    @Test
+    public void should_return_locker_full_and_capacity_is_0_when_locker_has_no_capacity() {
+        String expectTicket = "no capacity";
+
+        Locker locker = new Locker(0);
+
+        String ticket = locker.store();
+        Assert.assertEquals(expectTicket, ticket);
+        Assert.assertEquals(0, locker.getCapacity());
     }
 }
